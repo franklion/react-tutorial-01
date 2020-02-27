051b161d633f7ac6b0b499436612668d15881070
frank <frank.chen@skylinetw.com>
2020-02-27 16:02:54 +0800

    init: 初始化 React & 安裝 sass

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./page/home/sass/index.sass";
+import Index from "./page/home/index";
+
+ReactDOM.render(<Index />, document.getElementById("root"));
---
+++ src/page/home/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const index = () => {
+  return <div className="title">Hello Index!</div>
+}
+
+export default index
